904e3ee9a68cd043f7ec09ec90d42fd1570b1021
akashUnknow <akash.unknow@gmail.com>
2026-08-15 18:55:48 +0530

    feat: add application routing foundation

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import PublicLayout from './layouts/PublicLayout'
+import UserLayout from './layouts/UserLayout'
+import AdminLayout from './layouts/AdminLayout'
+import HomePage from './pages/public/HomePage'
+import ExamsPage from './pages/public/ExamsPage'
+import TestsPage from './pages/public/TestsPage'
+import LoginPage from './pages/auth/LoginPage'
+import RegisterPage from './pages/auth/RegisterPage'
+import DashboardPage from './pages/user/DashboardPage'
+import AdminDashboardPage from './pages/admin/AdminDashboardPage'
+import NotFoundPage from './pages/public/NotFoundPage'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route element={<PublicLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/exams" element={<ExamsPage />} />
+        <Route path="/tests" element={<TestsPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+
+      <Route element={<UserLayout />}>
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Route>
+
+      <Route element={<AdminLayout />}>
+        <Route path="/admin" element={<AdminDashboardPage />} />
+      </Route>
+
+      <Route path="/home" element={<Navigate to="/" replace />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  )
+}
